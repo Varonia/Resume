b49f6b6e43480d32f565a1df2d253d52debbe324
Varonia <w87344666@126.com>
2018-05-21 23:14:33 +0800

    MVC化

--- js/message.js
+++ js/message.js
@@ -1,49 +1,79 @@
-var APP_ID = '0oLsn7Qg4fCRcC19zXSP33xw-gzGzoHsz';
-var APP_KEY = 'Hre7IbvbdizLhL6RtIBAWL77';
-
-AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
-});
-
-var query = new AV.Query('Message');
-
-query.find()
-    .then(
-        function(messages) {
-            let array = messages.map((item) => item.attributes)
-            array.forEach((item) => {
-                let li = document.createElement('li')
-                li.innerText = `${item.name}:${item.content}`
-                let messageList = document.querySelector('#messageList')
-                messageList.appendChild(li)
+! function() {
+    var model = {
+        // 获取数据
+        init: function() {
+            var APP_ID = '0oLsn7Qg4fCRcC19zXSP33xw-gzGzoHsz';
+            var APP_KEY = 'Hre7IbvbdizLhL6RtIBAWL77';
+            AV.init({ appId: APP_ID, appKey: APP_KEY })
+        },
+        fetch: function() {
+            var query = new AV.Query('Message');
+            return query.find() // Promise 对象
+        },
+        // 创建数据
+        save: function(name, content) {
+            var Message = AV.Object.extend('Message');
+            var message = new Message();
+            return message.save({ // Promise 对象
+                'name': name,
+                'content': content
             })
         }
-    )
+    }
 
-let myForm = document.querySelector('#postMessageForm')
-myForm.addEventListener('submit', function(e) {
-    e.preventDefault()
-    let name = myForm.querySelector('input[name=name]').value
-    let content = myForm.querySelector('input[name=content]').value
-        // 声明类型
-    var Message = AV.Object.extend('Message');
-    // 新建对象
-    var message = new Message();
-    message.save({
-        content: content,
-        name: name
-    }).then(function(object) {
-        console.log('1')
-        let li = document.createElement('li')
-        console.log('2')
-        li.innerText = `${object.attributes.name}:${object.attributes.content}`
-        console.log('3')
-        let messageList = document.querySelector('#messageList')
-        console.log('4')
-        messageList.appendChild(li)
-        console.log('5')
-        myForm.querySelector('inpupt[name=content]').value = ''
-        console.log(object)
-    })
-})
+    var view = document.querySelector('section.message')
+
+
+    var controller = {
+        view: null,
+        model: null,
+        messageList: null,
+        init: function(view, model) {
+            this.view = view
+            this.model = model
+
+            this.messageList = view.querySelector('#messageList')
+            this.form = view.querySelector('form')
+            this.model.init()
+            this.loadMessages()
+            this.bindEvents()
+        },
+        loadMessages: function() {
+            this.model.fetch().then(
+                (messages) => {
+                    let array = messages.map((item) => item.attributes)
+                    array.forEach((item) => {
+                        let li = document.createElement('li')
+                        li.innerText = `${item.name}: ${item.content}`
+                        this.messageList.appendChild(li)
+                    })
+                }
+            )
+        },
+        bindEvents: function() {
+            let myForm = this
+            this.form.addEventListener('submit', function(e) {
+                e.preventDefault()
+                myForm.saveMessage()
+            })
+        },
+        saveMessage: function() {
+            let myForm = this.form
+            let content = myForm.querySelector('input[name=content]').value
+            let name = myForm.querySelector('input[name=name]').value
+            this.model.save(name, content).then(function(object) {
+                let li = document.createElement('li')
+                li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+                let messageList = document.querySelector('#messageList')
+                messageList.appendChild(li)
+                myForm.querySelector('input[name=content]').value = ''
+                console.log(object)
+            })
+        }
+
+    }
+
+    controller.init(view, model)
+
+
+}.call()
