9564f740fa7a5fb3e4eb2a10c22cb4d3217f27f3
varonia <w87344666@126.com>
2018-12-13 22:18:58 +0800

    修改优化

--- js/smoothly-navigation.js
+++ js/smoothly-navigation.js
@@ -18,10 +18,10 @@
         scrollToElement: function(element) {
             let top = element.offsetTop
             let currentTop = window.scrollY
-            let targetTop = top - 80
+            let targetTop = top - 120
             let s = targetTop - currentTop // 路程
             var coords = { y: currentTop }; // 起始位置
-            var t = Math.abs((s / 100) * 300) // 时间
+            var t = Math.abs((s / 100) * 300) // 时间 
             if (t > 500) { t = 500 }
             var tween = new TWEEN.Tween(coords) // 起始位置
                 .to({ y: targetTop }, t) // 结束位置 和 时间
